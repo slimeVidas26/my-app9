bf5238406a956709371f6b75f0c5fb5c99e04e6f
slimeVidas26 <dahan.isaac@gmail.com>
2024-05-11 23:25:06 +0300

    tabs

--- navigation/stackNavigators/EDI/EdiOrderDetailsStackNavigator.js
+++ navigation/stackNavigators/EDI/EdiOrderDetailsStackNavigator.js
@@ -12,37 +12,71 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 const Stack = createStackNavigator()
 const Tab = createMaterialTopTabNavigator();
 
+// function  MyTab(){
+//   return(
+// <View style = {{flex:1,flexDirection:'row' ,justifyContent:'space-between', alignItems:'center',backgroundColor:'red'}}>
+// <View style={styles.circle}><Text style={styles.textCircle}>25</Text></View>
+// <Text>Open</Text>
+// </View>)
+// }
+
 
 function MyTabBar({ navigation }) {
   return (
     <Tab.Navigator
+    initialRouteName='EdiOrderDetailsScreenOpen'
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+      showLabel: true,
+      //style: {backgroundColor:'red'},
+    }}
     screenOptions={{
-      //tabBarLabelStyle: { fontSize: 18 },
-      tabBarActiveTintColor: '#000',
-      //tabBarItemStyle: { width: 60 },
-     
-      ///tabBarGap: 10,
-      
-      // tabBarActiveTintColor: '#e91e63',
+      //tabBarActiveTintColor: "red",
+      //tabBarInactiveTintColor: "blue",
+      // tabBarStyle: {
+      //   height: 55,
+      // },
+      tabBarLabelStyle: {
+        fontSize: 18,
+        margin: 0,
+      },
+      tabBarStyle: {
+        //height: 90,
+        //paddingHorizontal: 5,
+        //paddingTop: 0,
+        //backgroundColor: 'rgba(34,36,40,1)',
+        //position: 'absolute',
+        //borderTopWidth: 0,
+        //top:500
+    },
     }}
     >
-            <Tab.Screen
-                options={{ tabBarLabel: ()=><Ionicons name="search-circle-sharp" size={48} color="blue" /> }} 
+            <Tab.Screen options={{ tabBarLabel: ()=><Ionicons name="search-circle-sharp" size={48} color="blue" /> }} 
                         name="EdiOrderDetailsScreenSearch" 
                         component={EdiOrderDetailsScreenSearch} />
 
       <Tab.Screen   options={{
-      tabBarLabel: ()=> <View style = {{flexDirection:'row' ,justifyContent:'space-between', alignItems:'center'}}>
-        <View style={styles.circle}><Text style={styles.textCircle}>25</Text></View>
-        <View><Text>Open</Text></View>
-        </View>}} 
+          tabBarLabel: ({focused, color, size , backgroundColor}) => (
+            <View style = {{backgroundColor: focused ? 'blue' : backgroundColor,flex:1 ,flexDirection:'row' ,justifyContent:'space-evenly', alignItems:'center',width:'100%' , borderRadius:10 }}>
+            <View style={[styles.circle , {backgroundColor: focused ? 'white' : 'blue'}]}><Text style={[styles.textCircle,{color: focused ? 'blue' : 'white'}]}>25</Text></View>
+            <Text style={{color: focused ? 'white' : color,fontWeight: 'bold',fontSize:18}}>Open</Text>
+
+            </View>
+          ) 
+        }}
          name="EdiOrderDetailsScreenOpen" component={EdiOrderDetailsScreenOpen} />
 
-      <Tab.Screen    options={{
-     tabBarLabel: ()=> <View style = {{flex:1 ,flexDirection:'row' ,justifyContent:'space-between', alignItems:'center'}}>
-     <View style={styles.circle}><Text style={styles.textCircle}>25</Text></View>
-     <View><Text>Closed</Text></View>
-     </View> }} name="EdiOrderDetailsScreenClosed" component={EdiOrderDetailsScreenClosed} />
+<Tab.Screen   options={{
+          tabBarLabel: ({focused, color, size , backgroundColor}) => (
+            <View style = {{backgroundColor: focused ? 'blue' : backgroundColor,flex:1 ,flexDirection:'row' ,justifyContent:'space-evenly', alignItems:'center',width:'100%' , borderRadius:10}}>
+            <View style={[styles.circle , {backgroundColor: focused ? 'white' : 'blue' , borderRadius:50}]}><Text style={[styles.textCircle,{color: focused ? 'blue' : 'white'}]}>25</Text></View>
+            <Text style={{color: focused ? 'white' : color,fontWeight: 'bold',fontSize:18}}>Closed</Text>
+
+            </View>
+          ) 
+        }}
+     name="EdiOrderDetailsScreenClosed" component={EdiOrderDetailsScreenClosed} />
 
     </Tab.Navigator>
     
@@ -55,7 +89,7 @@ function MyTabBar({ navigation }) {
 
 const EdiOrderDetailsStackNavigator = () => {
   return (
-     <Stack.Navigator screenOptions={{headerShown: false,}}>
+     <Stack.Navigator>
        <Stack.Screen name="MyTabBar" component={MyTabBar} />
      </Stack.Navigator>
    
@@ -65,6 +99,21 @@ const EdiOrderDetailsStackNavigator = () => {
 }
 
 const styles = StyleSheet.create({
+
+  tabContainer: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: 70,
+    //backgroundColor: '#F8F7FB',
+     backgroundColor: 'yellow',
+    borderRadius: 10,
+    elevation: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
   TextInputTab:{
     display:'flex',
     justifyContent :'center',
@@ -80,7 +129,7 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     padding: 10,
-    backgroundColor:'blue',
+    //backgroundColor:'blue',
     //border: '3px solid #000',
     //color: '#000',
     //textAlign: 'center',
